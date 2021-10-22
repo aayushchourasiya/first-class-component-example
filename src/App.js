795a0d3af832874b857de051ab49b364874ecd82
Aayush Chourasiya <aayushchourasiya@outlook.com>
2021-10-22 19:38:51 +0530

    First Class Component Example!

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar'
+
+class App extends React.Component{
+  render(){
+    return(
+      <Navbar nav="Title Here!"/>
+    );
+  }
+}
+
+export default App;
